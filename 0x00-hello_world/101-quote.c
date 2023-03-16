65b4c2112512765d58c5c80651061d363e0b4586
ahmedelqori <aelqori1@gmail.com>
2023-03-16 13:25:07 -0700

    ett

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+ * main - without puts and printf
+ * Return: 1
+ */
+
 int main(void)
 {
 char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
