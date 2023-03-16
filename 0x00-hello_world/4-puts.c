7fd163a79627fe58485e908eb067293a4c32a086
ahmedelqori <aelqori1@gmail.com>
2023-03-16 04:40:03 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
-/*
- *  main - print line with puts
-  */
-
+/**
+ * main - print line with puts
+ * Return: 0 (approved)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
