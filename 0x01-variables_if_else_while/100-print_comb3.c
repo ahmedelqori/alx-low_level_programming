4946eba61d540795e4c43b2269c0b19462204205
ahmedelqori <aelqori1@gmail.com>
2023-03-17 12:35:03 -0700

    test

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main - without printf
+ *Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (a < 10)
+	{
+		while (b < 10)
+		{
+			if (a < b)
+			{
+			putchar(48 + a);
+			putchar(48 + b);
+			putchar(',');
+			putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+
+}
