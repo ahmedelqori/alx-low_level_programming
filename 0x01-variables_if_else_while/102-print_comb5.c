b6cc9442d7b06712bca1bfc386233acdcda5d55f
ahmedelqori <aelqori1@gmail.com>
2023-03-17 17:04:23 -0700

    test

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,23 +15,28 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-
 		for (b = 0; b < 10; b++)
 		{
-
 			for (c = 0; c < 10; c++)
 			{
 				for (d = 0; d < 10; d++)
 				{
-					if (a + b < c + d)
+					int e, f;
+
+					e = a * 10 + b;
+					f = c * 10 + d;
+					if (e < f)
 					{
 					putchar(48 + a);
 					putchar(48 + b);
 					putchar(' ');
 					putchar(48 + c);
 					putchar(48 + d);
+					if (e != 98)
+					{
 					putchar(',');
 					putchar(' ');
+					}
 					}
 
 				}
