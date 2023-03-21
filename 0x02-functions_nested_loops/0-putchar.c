4bd651a0d599a7ab7d19c8108f98464d99e05f87
ahmedelqori <aelqori1@gmail.com>
2023-03-21 04:38:45 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
- *main - make putchar
- *Return: 0
-*/
+ *main- print putchar
+ *Return:0
+ */
+
 int main(void)
 {
-	printf("_putchar\n");
+
+	char string[] = "_putchar";
+
+	int letter = 0;
+
+	while (letter < 8)
+	{
+	_pucthar(letter);
+	}
+	_putchar('\n');
 	return (0);
 }
