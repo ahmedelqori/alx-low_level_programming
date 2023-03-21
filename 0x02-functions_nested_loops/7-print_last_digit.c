ca18a8eb60e2ccbed2b152f6439e42285168a171
ahmedelqori <aelqori1@gmail.com>
2023-03-21 06:04:18 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_last_digit - clear
+ *@n : input
+ *Return: 0
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	if (n < 0)
+		n *= (-1);
+		x %= n;
+	else
+		x %= n;
+_putchar(48 + x);
+return (x);
+}
