2ffa3d12711dac623cc8aac4abb33a19e04d7afa
ahmedelqori <aelqori1@gmail.com>
2023-03-21 05:06:32 -0700

    test

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isLower - check if letter is lower case
+ *@index: input
+ * Return: 0 || 1
+ */
+
+int _isLower(int index)
+{
+	if (index >= 65 && index <= 91)
+	{
+		return (0);
+	}
+	return (1);
+}
