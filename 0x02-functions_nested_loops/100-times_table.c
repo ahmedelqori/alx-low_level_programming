bfd491f3171eaefdfff6b33cc36399311c8767e9
ahmedelqori <aelqori1@gmail.com>
2023-03-21 13:32:55 -0700

    tets

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,11 @@ void print_times_table(int n)
 	{
 		int i, j;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			int a = 0;
 
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				printf("%d", a);
 
