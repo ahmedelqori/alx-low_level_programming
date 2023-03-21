fa9a7fa4a13fb8b50ca904c3de1292191778b382
ahmedelqori <aelqori1@gmail.com>
2023-03-21 04:26:13 -0700

    test

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet -  print alphabet
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		_putchar('\n');
+		x++;
+	}
+}
