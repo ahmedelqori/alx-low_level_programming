b7963b9c2d80fcd98261d060758355e221804aa5
ahmedelqori <aelqori1@gmail.com>
2023-03-21 04:22:28 -0700

    test

--- 101-quote.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main(void)
-{
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(STDOUT_FILENo, text, sizeof(text) - 1);
-return (1);
-}
