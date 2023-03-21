f26153222fe9b057da4525db012c9e0c625fcb7b
ahmedelqori <aelqori1@gmail.com>
2023-03-21 04:53:08 -0700

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - print alphabet ten times
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		print_alphabet();
+	i++;
+	}
+}
