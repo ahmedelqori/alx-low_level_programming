0ab171e786af34810986a2c2cb4921b57c7a2480
ahmedelqori <aelqori1@gmail.com>
2023-03-23 16:31:13 -0700

    9-fizz_bzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,17 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (i % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (i % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d ", i);
-		}
 		if (i < 100)
-		{
 			printf(" ");
-		}
+		else
+			continue;
 	}
 	printf("/n");
 	return (0);
