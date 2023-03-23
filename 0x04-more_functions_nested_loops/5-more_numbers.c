69ecf877fc1db1eca2ad5ba5f493d3153df7829c
ahmedelqori <aelqori1@gmail.com>
2023-03-23 10:10:12 -0700

    test

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *more_numbers - print form 0 to 14 ten times
+ *Return: Void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			if (j < 10)
+			{
+				_putchar(48 + j / 10);
+				_putchar(48 + j % 10);
+			}
+			else
+			{
+				_putchar(48 + j);
+			}
+		j++;
+		}
+		_putchar('\n');
+	i++;
+	}
+}
