d6a42cec8af973c62acb39eac4c1f81ae34beea9
ahmedelqori <aelqori1@gmail.com>
2023-03-23 09:56:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - print from 0 to 9
+ *Return: void
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar(48 + i);
+		i++
+	}
+}
