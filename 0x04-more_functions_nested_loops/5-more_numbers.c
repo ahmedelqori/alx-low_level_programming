73c96c8d4aaae0411f6a081a76b9bc0a106afd99
ahmedelqori <aelqori1@gmail.com>
2023-03-23 15:27:30 -0700

    tets

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,12 +19,8 @@ void more_numbers(void)
 			if (j > 9)
 			{
 				_putchar(48 + j / 10);
+			}
 				_putchar(48 + j % 10);
-			}
-			else
-			{
-				_putchar(48 + j);
-			}
 		}
 		_putchar('\n');
 	}
