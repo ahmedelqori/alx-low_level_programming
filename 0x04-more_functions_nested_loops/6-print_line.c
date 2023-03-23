732c1846420463879a1073e4b8ffe5f2f27dae96
ahmedelqori <aelqori1@gmail.com>
2023-03-23 14:49:05 -0700

    test

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,10 +14,14 @@ void print_line(int n)
 
 		i = 0;
 
-		for (i = 0; i < n ;i++)
+		for (i = 0; i < n ; i++)
 		{
 			_putchar('_');
 		}
-	}
 	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
