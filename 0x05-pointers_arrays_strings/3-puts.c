e1dca5525efd15b6d4a4af343a556857c8f556f1
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-27 21:33:45 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts - print a string
+*@str:input
+*Return:void
+*/
+
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        puts(str[i]);
+    }
+}
