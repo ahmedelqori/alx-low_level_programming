7a459b0f42465b8e3967c3d64d6930ce40d7804a
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-27 22:59:32 +0000

    puts half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*puts_half - print half of string
+*@str:input
+*Return: void
+*/
+
+void puts_half(char *str)
+{
+int length = 0;
+
+while (str[length])
+{
+length++;
+}
+length /= 2;
+while (str[length])
+{
+_putchar(str[length]);
+length++;
+}
+}
