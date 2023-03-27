753b6bacc1ac622ab3a2a7639f08daa1a1a3c03f
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-27 21:38:06 +0000

    delete spaces

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,12 @@
 
 void _puts(char *str)
 {
-    int i = 0;
+int i = 0;
 
-    while (str[i])
-    {
-        putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
+while (str[i])
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
 }
