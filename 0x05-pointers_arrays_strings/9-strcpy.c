9166ad4ed81827c4d189022652b1f6d8b6d039d9
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-28 00:42:14 +0000

    try

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,6 +9,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
+int i, length;
+for (length = 0; src[length] != '\0'; length++)
+{
+}
+for (i = 0; i <= length ; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+/*
 int length;
 
 length = 0;
@@ -18,4 +28,5 @@ dest[length] = src[length];
 length++;
 }
 return (dest);
+*/
 }
