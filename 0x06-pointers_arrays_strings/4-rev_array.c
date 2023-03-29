47f7ddeef74ee26f47ca439bf523c82535473cdf
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-29 23:14:24 +0000

    fixing beety

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,15 +12,15 @@ void reverse_array(int *a, int n)
 int i, j;
 
 i = 0;
-while (a[i])
+while (a[i] != '\0')
 {
-    i++;
+i++;
 }
 for (j = 0 ; j < (i / 2); j++)
 {
 int tmp = a[j];
 
-a[j] = a[i-1-j];
-a[i-1-j] = tmp;
+a[j] = a[i - 1 - j];
+a[i - 1 - j] = tmp;
 }
 }
