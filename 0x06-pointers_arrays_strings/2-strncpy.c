ff40b56ea9caeddc90fd3268cdef53ce46540fc6
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-29 22:47:11 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,21 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0;
+	int i;
 
-while (dest[i] && i < n)
-{
-dest[i] = src[i];
-i++;
-}
-return (dest);
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
