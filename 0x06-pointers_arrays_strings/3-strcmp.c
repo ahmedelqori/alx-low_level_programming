9679f44a3e4b512e221d3aa8df053de583bb1557
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-29 23:07:04 +0000

    fix beauty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,9 +2,8 @@
 
 /**
 * _strcmp - cpy string
-* @dest:input
-* @src:input
-* @n:input
+* @s1:input
+* @s2:input
 * Return: char
 */
 
@@ -14,7 +13,8 @@ int res, i;
 
 res = 0;
 i = 0;
-while (s1[i] && s1[i] == s2[i]){
+while (s1[i] && s1[i] == s2[i])
+{
 i++;
 }
 res = s1[i] - s2[i];
