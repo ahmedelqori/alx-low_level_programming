7977a8358bab6b35c39eebb034e7c95407c22ef2
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-29 23:12:07 +0000

    reverse_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* _strcmp - cpy string
+* @a:input
+* @n:input
+* Return: char
+*/
+
+void reverse_array(int *a, int n)
+{
+int i, j;
+
+i = 0;
+while (a[i])
+{
+    i++;
+}
+for (i = 0 ; j < (i / 2); j++)
+{
+int tmp = a[j];
+
+a[j] = a[i-1-j];
+a[i-1-j] = tmp;
+}
+}
