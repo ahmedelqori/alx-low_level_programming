3637c58436fbc97ee199c240d6524d56ae51601b
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-31 00:22:31 +0000

    fix for m github

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,38 +2,37 @@
 
 /**
 * cap_string - Upper String
-* @str:input
+* @s:input
 * Return: char*
 */
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-int i = 0;
+	int i;
 
-while (str[i])
-{
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-if (str[i - 1] == '\n' ||
-str[i - 1] == ' ' || str[i - 1] == '.'
-|| str[i - 1] == '\t')
-{
-char leL = 'a';
-
-char leU = 'A';
-
-while (leL <= 'z')
-{
-if (str[i] == leL)
-{
-str[i] = leU;
-}
-leU++;
-leL++;
-}
-}
-}
-i++;
-}
-return (str);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] = s[i] - 32;
+		}
+		else if ((s[0] >= 97 && s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
+		}
+		else
+		{
+			s[i] = s[i];
+		}
+		i++;
+	}
+	return (s);
 }
