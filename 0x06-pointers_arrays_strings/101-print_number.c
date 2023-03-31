3ebc1050d8574eb07dd6a2403f5577ebbfbe5dce
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-31 14:02:46 +0000

    fix betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,29 +8,29 @@
 
 void print_number(int n)
 {
-    int i;
-    
-    if (n == 0)
-    {
-        _putchar(48 + 0);
-    }
-    i = 1;
-    if (n < 0)
-    {
-        n *= -1;
-        _putchar('-');
-    }
-    while (i <= n)
-    {
-        i *= 10;
-    }
-    i /= 10;
-    while (n > 0)
-    {
-        int j = n / i;
+int i;
+
+if (n == 0)
+{
+_putchar(48 + 0);
+}
+i = 1;
+if (n < 0)
+{
+n *= -1;
+_putchar('-');
+}
+while (i <= n)
+{
+i *= 10;
+}
+i /= 10;
+while (n > 0)
+{
+int j = n / i;
         
-        _putchar(48 + j);
-        n = n % i;
-        i /= 10;
-    }
+_putchar(48 + j);
+n = n % i;
+i /= 10;
+}
 }
