8df38203c332209288cc2532764cc8a092976843
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-03-31 00:01:52 +0000

    cap String

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* string_toupper - Upper String
+* @str:input
+* Return: char*
+*/
+
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+if (str[i-1] == '\n' || str[i-1] == ' ' || str[i-1] == '.' || str[i-1] == '\t')
+{  
+char letterLower = 'a';
+
+char letterUpper = 'A';
+
+while (letterLower <= 'z')
+{  
+if (str[i] == letterLower)
+{
+str[i] = letterUpper;
+}
+letterUpper++;
+letterLower++;
+}      
+}
+} 
+i++;
+}
+}
