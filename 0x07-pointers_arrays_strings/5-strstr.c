16cc9efd549377a80cb51f5de877a813f6f23db7
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-04-03 23:05:56 +0000

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,25 +9,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-int i, j;
+unsigned int i = 0, j = 0;
 
-i = 0;
 while (haystack[i])
 {
-if (needle[0] == haystack[i]){
+while (needle[j] && (haystack[i] == needle[0]))
+{
+if (haystack[i + j] == needle[j])
+j++;
+else
 break;
 }
+if (needle[j])
+{
 i++;
-}
 j = 0;
-while (needle[j])
-{
-if (haystack[i] != needle[j])
-{
-return (NULL);
 }
-i++;
-j++;
+else
+return (haystack + i);
 }
-return (needle);
+return (0);
 }
