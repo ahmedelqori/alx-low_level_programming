56d5d9a447e7d399ead1b31c7bb536ea30f14227
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-04-05 21:17:17 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts word with recursion
+ * @s: input;
+ * Return : void
+ */
+
+void _puts_recursion(char *s)
+{
+if (s[0])
+{
+printf("%c",s[0]);
+_puts_recursion(s+1);
+}
+}
