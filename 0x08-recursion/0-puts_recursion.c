f0be7596207ffb85eb799ced0a7cee36726c93e6
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-04-05 21:32:01 +0000

    add new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-if (s[0])
+if (*s != '\0')
 {
-printf("%c", s[0]);
+_putchar(*s);
 _puts_recursion(s + 1);
 }
-if (s[0] == '\0')
+else
 {
 _putchar('\n');
 }
+
 }
