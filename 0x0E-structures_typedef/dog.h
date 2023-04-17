752f769ac714a306d6fb79f36291716569eb3df8
Ahmed EL-Qori <100059201+ahmedelqori@users.noreply.github.com>
2023-04-17 23:04:39 +0000

    final desicion

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - create struct
+ * @name: name
+ * @age: age
+ * @owner: Owner
+*/
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+
+#endif
