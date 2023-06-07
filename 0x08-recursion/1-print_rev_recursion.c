7a8a54b37ff6bd9864e107ce09a04829e6b836a1
ahmedelqori <aelqori1@gmail.com>
2023-06-07 16:48:37 +0100

    revrese

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -  print string reverese
+ * @s : string
+ * Return : void
+ */
+
+void _print_rev_recursion(char *s)
+{
+    if(*s == '\0') return;
+    if(*s)
+    {
+       _print_rev_recursion(s + 1);
+        _putchar(*s);
+    }
+}
