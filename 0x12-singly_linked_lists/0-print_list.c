eed879cfc8f6f78d1e25f8637896b4db1a2078ed
ahmedelqori <aelqori1@gmail.com>
2023-06-30 12:51:58 +0100

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *print_list - print node in this list
+ *@h: linked list
+ *Return: Length  of List
+ */
+size_t print_list(const list_t *h)
+{
+	int length = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
